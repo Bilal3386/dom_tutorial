dab471cc1571b348a11ee3e57960f9dd89344c0f
mohdbilal <mohdbilal3386@gmail.com>
2022-07-09 03:06:16 +0530

    some changes in main js

---
+++ main.js
@@ -0,0 +1,5 @@
+let title = document.getElementById('main-header')
+let item = document.getElementsByClassName('list-group-item')
+title.style.borderBottom = "3px solid black"
+item[1].style.fontWeight = "bold"
+item[1].style.color = "green"
