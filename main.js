e19f63154c9ac05ce9351cda7a3fc0deb87ac98d
mohdbilal <mohdbilal3386@gmail.com>
2022-07-09 10:42:46 +0530

    getElementsByTagName

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-let title = document.getElementById('main-header')
-let item = document.getElementsByClassName('list-group-item')
-title.style.borderBottom = "3px solid black"
-item[1].style.fontWeight = "bold"
-item[2].style.background = "green"
-for(let i=0; i<item.length; i++)
+// let title = document.getElementById('main-header')
+// let item = document.getElementsByClassName('list-group-item')
+// title.style.borderBottom = "3px solid black"
+// item[1].style.fontWeight = "bold"
+// item[2].style.background = "green"
+// for(let i=0; i<item.length; i++)
+//  {
+//     item[i].style.fontWeight = 'bold'
+//  }
+
+
+let li = document.getElementsByTagName('li')
+li[1].style.fontWeight = "bold"
+li[2].style.background = "green"
+for(let i=0; i<li.length; i++)
  {
-    item[i].style.fontWeight = 'bold'
+    li[i].style.fontWeight = 'bold'
  }
