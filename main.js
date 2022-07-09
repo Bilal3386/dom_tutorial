dfc6588c822cca7990b40097a16a63ca1d779c08
mohdbilal <mohdbilal3386@gmail.com>
2022-07-09 19:55:48 +0530

    Delete and Edit Functionality

--- main.js
+++ main.js
@@ -36,7 +36,7 @@
 // }
 
 // Manipulate the DOM or Traversing the DOM
-let itemList = document.querySelector('#items')
+// let itemList = document.querySelector('#items')
 
 
                                 /* PARENT AND CHILD START */ 
@@ -93,34 +93,98 @@ let itemList = document.querySelector('#items')
                                  /* CREATING ELEMENTS AND INSERTING ELEMENT */ 
 
 // create a div 
-let  newDiv = document.createElement('div')
+// let  newDiv = document.createElement('div')
 
-// add class
-newDiv.className = "hello"
+// // add class
+// newDiv.className = "hello"
 
-// add id
-newDiv.id = "hello1"
+// // add id
+// newDiv.id = "hello1"
 
-// add attr
-newDiv.setAttribute('title', 'hello div')
+// // add attr
+// newDiv.setAttribute('title', 'hello div')
 
-// create  text  node 
-let newDivTxt = document.createTextNode("hello World")
+// // create  text  node 
+// let newDivTxt = document.createTextNode("hello World")
 
-//add text to div
-newDiv.appendChild(newDivTxt)
+// //add text to div
+// newDiv.appendChild(newDivTxt)
 
-// add font size
-newDiv.style.fontSize = "30px"
-let container = document.querySelector('header .container')
-let h1 = document.querySelector('header h1')
-container.insertBefore(newDiv, h1)
+// // add font size
+// newDiv.style.fontSize = "30px"
+// let container = document.querySelector('header .container')
+// let h1 = document.querySelector('header h1')
+// container.insertBefore(newDiv, h1)
 
-console.log(newDiv);
+// console.log(newDiv);
 
-// add HEllo word before Item 1
-let itemDiv = document.createElement('div')
-let itemDivText = document.createTextNode("Hello word")
-itemDiv.appendChild(itemDivText)
-let item = document.querySelector('#items')
-item.insertBefore(itemDiv, item.firstElementChild)
+// // add HEllo word before Item 1
+// let itemDiv = document.createElement('div')
+// let itemDivText = document.createTextNode("Hello word")
+// itemDiv.appendChild(itemDivText)
+// let item = document.querySelector('#items')
+// item.insertBefore(itemDiv, item.firstElementChild)
+
+
+                        /* Delete and Edit Functionality */
+let form =  document.getElementById('addForm')
+let itemList = document.getElementById('items')
+
+// form submit event 
+form.addEventListener('submit', addItem)
+
+// Delete event
+itemList.addEventListener('click', removeItem)
+
+// Add Item
+function addItem(e) {
+    e.preventDefault();
+    
+    // Get input Value
+    let newItem = document.getElementById('item')
+
+    //  Create new li element
+    let li  = document.createElement('li')
+    // add class
+    li.className = 'list-group-item'
+
+    //Add textNode with Input Value 
+    li.appendChild(document.createTextNode(newItem.value))
+    
+    // create delete element button
+    let deleteBtn = document.createElement('button')
+    // add class
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete'
+    // Append textNode
+    deleteBtn.appendChild(document.createTextNode('X'))
+
+    // Append button to li 
+    li.appendChild(deleteBtn)
+
+
+    // create edit element button
+    let editBtn = document.createElement('button')
+    // add class
+    editBtn.className = 'btn btn-info btn-sm float-right mr-2 edit'
+    // Append textNode
+    editBtn.appendChild(document.createTextNode('edit'))
+
+    // Append button to li 
+    li.appendChild(editBtn)
+
+    // Append li to list
+    itemList.appendChild(li)
+}
+
+
+// Remove item 
+function removeItem (e) {
+    if(e.target.classList.contains('delete'))
+    {
+        if(confirm("Are You Sure"))
+        {
+            let li = e.target.parentElement
+            itemList.removeChild(li)
+        }
+    }
+}
