e01a8db71f381927383dff098e49cdaed649f01c
mohdbilal <mohdbilal3386@gmail.com>
2022-07-09 11:33:02 +0530

    QuerySelector && QuerySelectorAll

--- main.js
+++ main.js
@@ -9,10 +9,29 @@
 //  }
 
 
-let li = document.getElementsByTagName('li')
-li[1].style.fontWeight = "bold"
-li[2].style.background = "green"
-for(let i=0; i<li.length; i++)
- {
-    li[i].style.fontWeight = 'bold'
- }
+// let li = document.getElementsByTagName('li')
+// li[1].style.fontWeight = "bold"
+// li[2].style.background = "green"
+// for(let i=0; i<li.length; i++)
+//  {
+//     li[i].style.fontWeight = 'bold'
+//  }
+
+ //QUERYSELECTOR AND QUERYSELECTORALL
+
+//querySelector
+//  let li = document.querySelector('.list-group-item:nth-child(2)')
+//  li.style.background = "green"
+//  let thirdLi = document.querySelector('.list-group-item:nth-child(3)')
+//  thirdLi.style.display = 'none'
+
+//querySelectorAll
+
+// let li = document.querySelectorAll('li')
+// li[1].style.background = "green"
+
+let odd = document.querySelectorAll('li:nth-child(odd)')
+for (let i = 0; i < odd.length; i++) {
+    odd[i].style.backgroundColor = "green"
+}
+
